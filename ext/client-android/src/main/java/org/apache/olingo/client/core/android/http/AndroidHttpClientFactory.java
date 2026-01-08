433f5e9e742ffac3142ff7be2beb70863eafee3c
indraninm-sap <indrani.n.m@sap.com>
2026-01-08 10:24:26 +0530

    update httpclient version

--- ext/client-android/src/main/java/org/apache/olingo/client/core/android/http/AndroidHttpClientFactory.java
+++ ext/client-android/src/main/java/org/apache/olingo/client/core/android/http/AndroidHttpClientFactory.java
@@ -19,25 +19,28 @@
 package org.apache.olingo.client.core.android.http;
 
 import java.net.URI;
-
-import org.apache.http.client.HttpClient;
+import java.io.IOException;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.olingo.client.core.http.AbstractHttpClientFactory;
 import org.apache.olingo.commons.api.http.HttpMethod;
 
-import android.net.http.AndroidHttpClient;
-
+/**
+ * Android-specific HTTP client factory. On non-Android platforms this factory is not usable;
+ * create() will throw UnsupportedOperationException to make this explicit at runtime.
+ */
 public class AndroidHttpClientFactory extends AbstractHttpClientFactory {
 
-    @Override
-    public AndroidHttpClient create(final HttpMethod method, final URI uri) {
-        return AndroidHttpClient.newInstance(USER_AGENT);
-    }
+  @Override
+  public HttpClient create(final HttpMethod method, final URI uri) {
+    // This factory is only meant for Android runtime where a compatible HTTP client exists.
+    throw new UnsupportedOperationException("AndroidHttpClientFactory is only available on Android runtime");
+  }
 
-    @Override
-    public void close(final HttpClient httpClient) {
-        if (httpClient instanceof AndroidHttpClient) {
-            ((AndroidHttpClient) httpClient).close();
-        }
+  @Override
+  public void close(final CloseableHttpClient httpClient) throws IOException {
+    if (httpClient != null) {
+      httpClient.close();
     }
-
+  }
 }
